69dd2ef6148bbe45ddc65fc812e600e33f5b295a
Dev Shah <shahdev99@gmail.com>
2024-07-06 12:01:07 -0400

    menu changes

--- script.js
+++ script.js
@@ -346,7 +346,7 @@ var interiorObserver = new IntersectionObserver(
     entries.forEach((entry) => {
       if (entry.isIntersecting) {
         entry.target.classList.add("active");
-        observer.unobserve(entry);
+        // observer.unobserve(entry);
       }
     });
   },
@@ -507,3 +507,6 @@ window.addEventListener("load", () => {
   setScrollVar();
   scrollAnimation();
 });
+
+
+// QR Code: <img src="https://chart.googleapis.com/chart?cht=qr&chl=https%3A%2F%2Fjd-designs.netlify.app%2F&chs=180x180&choe=UTF-8&chld=L|2" alt="qr code"><a href="www.qr-code-generator.com/" border="0" style="cursor:default" rel="nofollow"></a>
